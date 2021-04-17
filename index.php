2b7c5a7de2b8c9032a930fc2b2474ceda07a3425
Matthieu <matthieudejean@hotmail.com>
2021-04-18 00:45:16 +0200

    Oeil de Sauron

---
+++ index.php
@@ -0,0 +1,410 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="sheet.css">
+
+</head>
+<body>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-1 col-xs-1 bg-dark">
+            1
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            2
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            3
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            4
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            5
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            6
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            7
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            8
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            9
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            10
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            11
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1 col-xs-1 bg-dark">
+            1
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            2
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            3
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            4
+        </div>
+        <div class="col-1 col-xs-1 bg-danger">
+            5
+        </div>
+        <div class="col-1 col-xs-1 bg-primary">
+            6
+        </div>
+        <div class="col-1 col-xs-1 bg-danger">
+            7
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            8
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            9
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            10
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            11
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1 col-xs-1 bg-dark">
+            1
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            2
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            3
+        </div>
+        <div class="col-1 col-xs-1 bg-danger">
+            4
+        </div>
+        <div class="col-1 col-xs-1 bg-primary">
+            5
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            6
+        </div>
+        <div class="col-1 col-xs-1 bg-primary">
+            7
+        </div>
+        <div class="col-1 col-xs-1 bg-danger">
+            8
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            9
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            10
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            11
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1 col-xs-1 bg-dark">
+            1
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            2
+        </div>
+        <div class="col-1 col-xs-1 bg-danger">
+            3
+        </div>
+        <div class="col-1 col-xs-1 bg-danger">
+            4
+        </div>
+        <div class="col-1 col-xs-1 bg-primary">
+            5
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            6
+        </div>
+        <div class="col-1 col-xs-1 bg-primary">
+            7
+        </div>
+        <div class="col-1 col-xs-1 bg-danger">
+            8
+        </div>
+        <div class="col-1 col-xs-1 bg-danger">
+            9
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            10
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            11
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1 col-xs-1 bg-dark">
+            1
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            2
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            3
+        </div>
+        <div class="col-1 col-xs-1 bg-danger">
+            4
+        </div>
+        <div class="col-1 col-xs-1 bg-primary">
+            5
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            6
+        </div>
+        <div class="col-1 col-xs-1 bg-primary">
+            7
+        </div>
+        <div class="col-1 col-xs-1 bg-danger">
+            8
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            9
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            10
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            11
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1 col-xs-1 bg-dark">
+            1
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            2
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            3
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            4
+        </div>
+        <div class="col-1 col-xs-1 bg-danger">
+            5
+        </div>
+        <div class="col-1 col-xs-1 bg-primary">
+            6
+        </div>
+        <div class="col-1 col-xs-1 bg-danger">
+            7
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            8
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            9
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            10
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            11
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1 col-xs-1 bg-white">
+            1
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            2
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            3
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            4
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            5
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            6
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            7
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            8
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            9
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            10
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            11
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1 col-xs-1 bg-white">
+            1
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            2
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            3
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            4
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            5
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            6
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            7
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            8
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            9
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            10
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            11
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1 col-xs-1 bg-white">
+            1
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            2
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            3
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            4
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            5
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            6
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            7
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            8
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            9
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            10
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            11
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1 col-xs-1 bg-white">
+            1
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            2
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            3
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            4
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            5
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            6
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            7
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            8
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            9
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            10
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            11
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1 col-xs-1 bg-white">
+            1
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            2
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            3
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            4
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            5
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            6
+        </div>
+        <div class="col-1 col-xs-1 bg-dark">
+            7
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            8
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            9
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            10
+        </div>
+        <div class="col-1 col-xs-1 bg-white">
+            11
+        </div>
+    </div>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+</html>
+
+<?php
+
+
